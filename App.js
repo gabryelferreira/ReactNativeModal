93ed7a39034fffed3b0310669d82a52d7c59063d
Gabryel Ferreira <gabryel.ferreira@hotmail.com>
2019-01-13 16:16:02 -0200

    Modal use example

--- App.js
+++ App.js
@@ -1,49 +1,88 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { View, Text, TouchableOpacity, Alert } from 'react-native';
+import Modalzin from './src/components/Modal/Modal';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends Component {
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+  state = {
+    modal: {
+      visible: false,
+      title: "Modal title",
+      subTitle: "Modal subTitle",
+      buttons: this.createButtons()
+    }
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+  constructor(props){
+    super(props);
+  }
+
+  functionOnModalClose(key){
+    this.setState({
+      modal: {
+        visible: false
+      }
+    });
+    if (key == "BEST"){
+      Alert.alert("Hey bro!", "You just clicked in the best button EVER.");
+    } else {
+      Alert.alert("Ok...", "Not a good button to click.");
+    }
+  }
+
+  openModal(){
+    this.setState({
+      modal: {
+        visible: true,
+        title: "Modal title",
+        subTitle: "Modal subTitle",
+        buttons: this.createButtons()
+      }
+    })
+  }
+
+  createButtons(){
+    let buttons = [
+      {key: "BEST", text: "BEST button", color: '#27ae60', fontWeight: 'bold'},
+      {key: "WORST", text: "Not a good button"}
+    ];
+    return buttons;
+  }
+
+  render(){
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <Modalzin
+        visible={this.state.modal.visible}
+        title={this.state.modal.title}
+        subTitle={this.state.modal.subTitle}
+        buttons={this.state.modal.buttons}
+        onClick={(key) => this.functionOnModalClose(key)}
+      />
+        <TouchableOpacity onPress={() => this.openModal()} style={styles.button}>
+          <Text style={styles.buttonText}>Open modal</Text>
+        </TouchableOpacity>
       </View>
     );
   }
+
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+    borderRadius: 4,
+    backgroundColor: '#27ae60'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16
+  }
+}
